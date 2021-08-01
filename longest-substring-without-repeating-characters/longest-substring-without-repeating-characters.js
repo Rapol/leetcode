fa93e946f86ece10ef1e523e6ca84ebf010d3244
Rafael Pol <rafael.pol@protonmail.com>
2021-07-31 22:36:56 -0400

    Update longest-substring-without-repeating-characters.js

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -3,58 +3,17 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    const subStringMap = new Map();
-    const subIndexMap = new Map();
+    const subStringSet = new Set();
     let longestStreak = 0;
     let subStringStart = 0;
     for(let i = 0; i < s.length; i++) {
         const char = s[i];
-        if(subStringMap.get(char) !== undefined) {
-            let prevSubString = subStringStart; // 0
-            subStringStart = subStringMap.get(char) + 1; // 2
-            for(let j = prevSubString; j < subStringStart; j++) { // j = 0; j < 1
-                const charToDelete = subIndexMap.get(j);
-                subIndexMap.delete(j);
-                subStringMap.delete(charToDelete);
-            }
+        while(subStringSet.has(char)) {
+            subStringSet.delete(s[subStringStart])
+            subStringStart++;
         }
-        subStringMap.set(char, i);
-        subIndexMap.set(i, char);
-        const currentStreak = (i + 1) - subStringStart;
-        longestStreak = longestStreak > currentStreak ? longestStreak : currentStreak;
+        subStringSet.add(char);
+        longestStreak = Math.max(longestStreak, (i + 1) - subStringStart)
     }
     return longestStreak;
 };
-/**
-a
-i = 0
-longestStreak = 1;
-subStringStart = 0;
-map = 
-  a = 0
-----------------------
-b
-i = 1
-longestStreak = 2;
-subStringStart = 0;
-map = 
-  a = 0
-  b = 1
-----------------------
-b
-i = 2
-longestStreak = 2;
-subStringStart = 2;
-map = 
-  a = 0
-  b = 2
-----------------------
-a
-i = 3
-longestStreak = 2;
-subStringStart = 1;
-map = 
-  a = 3
-  b = 2
-----------------------
-*/
